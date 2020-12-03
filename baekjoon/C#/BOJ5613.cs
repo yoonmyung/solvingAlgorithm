189e54fce2730c78923b8fd98d2b450a5c0074a7
kym1229 <ymymym1229@gmail.com>
2020-12-03 23:33:38 +0900

    ~201203

---
+++ baekjoon/C#/BOJ5613.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//백준에서는 틀렸다고 나옴! 테스트 케이스 더 필요
+namespace BOJ
+{
+    class BOJ5613
+    {
+        public static void Main()
+        {
+            string input = "_";
+            char Operator = '$';
+            long operation1 = 111111111;
+            long operation2;
+
+            while (true)
+            {
+                input.Remove(0);
+                input = Console.ReadLine();
+                if (input.Contains('+') ||
+                   input.Contains('-') ||
+                   input.Contains('*') ||
+                   input.Contains('/'))
+                {
+                    Operator = input[0];
+                }
+                else if (input.Contains('='))
+                    break;
+                else
+                {
+                    if (operation1 < 111111111 && Operator != '$')
+                    {
+                        operation2 = int.Parse(input);
+                        switch (Operator)
+                        {
+                            case '+':
+                                operation1 += operation2;
+                                break;
+                            case '-':
+                                operation1 -= operation2;
+                                break;
+                            case '*':
+                                operation1 *= operation2;
+                                break;
+                            default:
+                                operation1 /= operation2;
+                                break;
+                        }
+                        Operator = '$';
+                    }
+                    else
+                        operation1 = int.Parse(input);
+                }
+            }
+            Console.WriteLine($"{operation1}");
+        }
+    }
+}
---
+++ baekjoon/C#/BOJ9654.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOJ
+{
+    class BOJ9654
+    {
+		public static void Main()
+		{
+			string fmt = "{0, -15} {1, -15} {2, -11} {3, -10}";
+
+			Console.WriteLine(fmt, "SHIP NAME", "CLASS", "DEPLOYMENT", "IN SERVICE");
+			Console.WriteLine(fmt, "N2 Bomber", "Heavy Fighter", "Limited", "21");
+			Console.WriteLine(fmt, "J - Type 327", "Light Combat", "Unlimited", "1");
+			Console.WriteLine(fmt, "NX Cruiser", "Medium Fighter", "Limited", "18");
+			Console.WriteLine(fmt, "N1 Starfighter", "Medium Fighter", "Unlimited", "25");
+			Console.WriteLine(fmt, "Royal Cruiser", "Light Combat", "Limited", "4");
+		}
+	}
+}
